d039956fac657cef92019b9b90c8ecf90821b62b
MasterSaberD1996 <134637529+MasterSaberD1996@users.noreply.github.com>
2023-05-26 21:37:07 -0600

    Tests and scripts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,32 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {AppComponent} from './app.component';
+import {CoreModule} from "./core/core.module";
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  });
+	let fixture: ComponentFixture<AppComponent>;
+	let component: AppComponent;
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [
+				RouterTestingModule,
+				CoreModule
+			],
+			declarations: [
+				AppComponent
+			],
+		}).compileComponents();
 
-  it(`should have as title 'JDMBlog'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('JDMBlog');
-  });
+		fixture = TestBed.createComponent(AppComponent);
+		component = fixture.componentInstance;
+	});
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('JDMBlog app is running!');
-  });
+	it('should create the app', () => {
+		expect(component).toBeTruthy();
+	});
+
+	it(`should have as title 'JDMBlog'`, () => {
+		expect(component.title).toEqual('JDMBlog');
+	});
 });
