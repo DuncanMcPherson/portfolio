86aba53063ff357bea54bd3a5eb3f2d3bb06ec55
Duncan McPherson <d.mcpherson.home@gmail.com>
2023-06-22 23:07:20 -0600

    just have 2 more files to fix

--- src/app/admin/components/edit-project-modal/edit-project-modal.component.spec.ts
+++ src/app/admin/components/edit-project-modal/edit-project-modal.component.spec.ts
@@ -1,23 +1,34 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { EditProjectModalComponent } from './edit-project-modal.component';
+import {EditProjectModalComponent} from './edit-project-modal.component';
+import {IProject} from "../../../projects/models/project.model";
+import {ProjectBuilder} from "../../../../test-utils/builders/project.builder";
+import {CoreModule} from "../../../core/core.module";
+import {ReactiveFormsModule} from "@angular/forms";
 
 describe('EditProjectModalComponent', () => {
-  let component: EditProjectModalComponent;
-  let fixture: ComponentFixture<EditProjectModalComponent>;
+	let component: EditProjectModalComponent;
+	let fixture: ComponentFixture<EditProjectModalComponent>;
+	let project: IProject;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ EditProjectModalComponent ]
-    })
-    .compileComponents();
+	beforeEach(async () => {
+	  	project = new ProjectBuilder().build();
+		await TestBed.configureTestingModule({
+			declarations: [EditProjectModalComponent],
+			imports: [
+				CoreModule,
+				ReactiveFormsModule
+			]
+		})
+			.compileComponents();
 
-    fixture = TestBed.createComponent(EditProjectModalComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+		fixture = TestBed.createComponent(EditProjectModalComponent);
+		component = fixture.componentInstance;
+		component.project = project;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
--- src/app/admin/components/project-card/project-card.component.spec.ts
+++ src/app/admin/components/project-card/project-card.component.spec.ts
@@ -1,23 +1,28 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { ProjectCardComponent } from './project-card.component';
+import {ProjectCardComponent} from './project-card.component';
+import {CoreModule} from "../../../core/core.module";
+import {ProjectBuilder} from "../../../../test-utils/builders/project.builder";
 
 describe('ProjectCardComponent', () => {
-  let component: ProjectCardComponent;
-  let fixture: ComponentFixture<ProjectCardComponent>;
+	let component: ProjectCardComponent;
+	let fixture: ComponentFixture<ProjectCardComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ProjectCardComponent ]
-    })
-    .compileComponents();
+	beforeEach(async () => {
+		let project = new ProjectBuilder().build();
+		await TestBed.configureTestingModule({
+			declarations: [ProjectCardComponent],
+			imports: [CoreModule]
+		})
+			.compileComponents();
 
-    fixture = TestBed.createComponent(ProjectCardComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+		fixture = TestBed.createComponent(ProjectCardComponent);
+		component = fixture.componentInstance;
+		component.project = project;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
