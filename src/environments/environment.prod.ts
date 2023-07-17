22dc7df16440607d0892311d53e810562af4b5e0
James D McPherson <139790207+DuncanMcPherson@users.noreply.github.com>
2023-07-17 11:36:31 -0600

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,3 @@
 export const environment = {
 	production: true,
-	firebaseInit: {
-		apiKey: "AIzaSyBSoW-VT4nZjscFDj_VKFRs4CTKdPJ50jQ",
-		authDomain: "jduncanm-blog.firebaseapp.com",
-		databaseURL: "https://jduncanm-blog-default-rtdb.firebaseio.com",
-		projectId: "jduncanm-blog",
-		storageBucket: "jduncanm-blog.appspot.com",
-		messagingSenderId: "641380028909",
-		appId: "1:641380028909:web:350355e70868fe322e57e4"
-	},
-	recaptchaKey: '6Leg82YmAAAAAHnnc8yHbToPfkECPp2zwWWtag25',
-	linkPreviewKey: '69fbc8c78c0063f2d0d995d9ef95532b'
 };
